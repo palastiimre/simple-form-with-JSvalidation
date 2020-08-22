7a23169558faacc26f076ca61e41d612fea17145
palasti_imre <palastiimre@gmail.com>
2020-08-22 21:12:41 +0200

    add validation(email,password,confirm password)

--- app.js
+++ app.js
@@ -21,10 +21,36 @@ function checkInputs() {
     const passwordValue = password.value.trim();
     const confirmPasswordValue = confirmPassword.value.trim();
 
+    //username field validation
     if (usernameValue === "") {
         setErrorClass(username, "Username cannot be blank");
     } else {
         setSuccessClass(username);
+    }
+
+    //email validation with regex
+    if (emailValue === "") {
+        setErrorClass(email, "Email is can't be blink");
+    } else if (!isEmail(emailValue)) {
+        setErrorClass(email, "Email is not valid");
+    } else {
+        setSuccessClass(email);
+    }
+
+    //password validation
+    if (passwordValue === "") {
+        setErrorClass(password, "Password cannot be blank");
+    } else {
+        setSuccessClass(password);
+    }
+
+    //confirm password validation
+    if (confirmPasswordValue === "") {
+        setErrorClass(confirmPassword, "The confirm password cannot be blank");
+    } else if (passwordValue !== confirmPasswordValue) {
+        setErrorClass(confirmPassword, "The confirm password does not match");
+    } else {
+        setSuccessClass(confirmPassword);
     }
 }
 
@@ -44,4 +70,11 @@ function setSuccessClass(input) {
 
     //add error class
     formControl.className = "form-control success";
+}
+
+//testing with regex the inptu email address
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email
+    );
 }
